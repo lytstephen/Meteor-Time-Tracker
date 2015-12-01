97e4af649f9ee93f0924392de20fc534c3a97e9f
Stephen Leung <stephen@spacious.hk>
2015-12-01 11:28:16 +0800

    project name read from collection and rename functionality

---
+++ client/templates/dashboard/dashboard.js
@@ -0,0 +1,51 @@
+Template.Dashboard.events({
+
+  'click #add-project': function(e) {
+    var projectName = $('#add-project-text').val();
+    var numProjects = Projects.find().count();
+
+    var doc = {
+      userId: Meteor.userId(),
+      name: projectName,
+      order: numProjects + 1
+    };
+
+    Projects.insert(doc);
+  },
+
+  'click .project-nav': function(e) {
+    e.preventDefault();
+
+    var currentProjectId = $(e.target).data('id');
+
+    Session.set('currentProjectId', currentProjectId);
+  },
+
+  'change #project-name': function(e) {
+    var newName = $(e.target).val();
+    var currentProjectId = Session.get('currentProjectId');
+    var operator = {$set: {name: newName}};
+
+    Projects.update(currentProjectId, operator);
+  }
+
+});
+
+Template.Dashboard.helpers({
+
+  projects: function() {
+    return Projects.find({archived: false}, {sort: {order: 1}});
+  },
+
+  currentProject: function() {
+    var currentProjectId = Session.get('currentProjectId');
+
+    if (currentProjectId) {
+      return Projects.findOne(currentProjectId);
+    } else {
+      return Projects.findOne({order: 1});
+    }
+
+  }
+
+});
